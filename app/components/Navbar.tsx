ea99530b423c428a567483c7dfa4e11f4a10d8c7
Oleksandr Berezhnyi <berrezhnoy@gmail.com>
2024-10-22 20:51:30 +0300

    Add search bar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Menu from "./Menu";
+import SearchBar from "./SearchBar";
+import NavIcons from "./NavIcons";
 
 const Navbar = () => {
   return (
@@ -15,14 +17,15 @@ const Navbar = () => {
 
       <div className="hidden md:flex items-center justify-between gap-8 h-full">
         <div className="w-1/3">
-          <Link href="/">
-            <div className="flex items-center justify-start gap-4">
-              <Image src="/logo.png" alt="" width={24} height={24} />
-              <div className="text-2xl tracking-wide">OB SHOP</div>
-            </div>
+          <Link href="/" className="flex items-center gap-3">
+            <Image src="/logo.png" alt="" width={24} height={24} />
+            <div className="text-2xl tracking-wide">OB SHOP</div>
           </Link>
         </div>
-        <div className="w-2/3"></div>
+        <div className="w-2/3 flex items-center justify-between gap-8">
+          <SearchBar />
+          <NavIcons />
+        </div>
       </div>
     </div>
   );
