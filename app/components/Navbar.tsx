2edd439d94a6e0c77f18ebf89ffd6a1792d7882a
Oleksandr Berezhnyi <berrezhnoy@gmail.com>
2024-10-22 20:27:10 +0300

    Add Navbar and mobile menu

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Menu from "./Menu";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+      <div className="md:hidden flex items-center justify-between h-full">
+        <Link href="/">
+          <div className="text-2xl tracking-wide">OB SHOP</div>
+        </Link>
+        <Menu />
+      </div>
+
+      <div className="hidden md:flex items-center justify-between gap-8 h-full">
+        <div className="w-1/3">
+          <Link href="/">
+            <div className="flex items-center justify-start gap-4">
+              <Image src="/logo.png" alt="" width={24} height={24} />
+              <div className="text-2xl tracking-wide">OB SHOP</div>
+            </div>
+          </Link>
+        </div>
+        <div className="w-2/3"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
